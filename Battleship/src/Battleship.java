b7bd2bb30b08b85cf53ea76bcf22c14edafcd5f2
Carson.Freeman <Carson.Freeman@MU-C1-08.mullen.pvt.k12.co.us>
2017-09-27 14:48:28 -0600

    Initial commit

---
+++ Battleship/src/Battleship.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class Battleship
+	{
+
+		public static void main(String[] args)
+			{
+				System.out.println("Welcome Officers to the Naval Waters!");
+				System.out.println();
+				System.out.println("Soon you will be directed to our maps, where you will position your ships!");
+				
+				ArrayList<String> shipPlacement = new ArrayList<String>();
+				shipPlacement.add(" ");
+				
+			}
+
+	}
---
+++ Battleship/src/BattleshipList.java
@@ -0,0 +1,19 @@
+
+public class BattleshipList
+	{
+		private String Yes;
+		
+		private String[][] PlacementBoard;
+		
+		private String[][] AtkBoard;
+		
+		private Arraylist<String> shipPlacement;
+		
+	}
+	public BattleshipList (String super)
+	{
+		PlacementBoard = new String [10][10];
+		
+		shipPlacement = new ArrayList<Playing>;
+		
+	}
