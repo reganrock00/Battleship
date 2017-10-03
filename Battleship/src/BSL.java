7271e98e1e28b691164e2f987a1e7fcdbd75c504
Carson.Freeman <Carson.Freeman@MU-C1-08.mullen.pvt.k12.co.us>
2017-10-03 09:05:39 -0600

    a

---
+++ Battleship/src/BSL.java
@@ -0,0 +1,71 @@
+
+
+public class BSL
+	{
+		private String Str1;
+		
+		private int place1;
+		
+		
+		
+		
+		
+		
+		public BSL (String Str1, int place1)
+			{
+				this.Str1 = Str1;
+				this.place1 = place1;
+				
+			}
+
+
+
+
+
+
+		public String getStr1()
+			{
+				return Str1;
+			}
+
+
+
+
+
+
+		public void setStr1(String str1)
+			{
+				Str1 = str1;
+			}
+
+
+
+
+
+
+		public int getPlace1()
+			{
+				return place1;
+			}
+
+
+
+
+
+
+		public void setPlace1(int place1)
+			{
+				this.place1 = place1;
+			}
+
+
+
+
+
+			
+	
+	}
+		
+		
+	
+	
